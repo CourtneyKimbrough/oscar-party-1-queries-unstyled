e61dc354862d4478cc00657d3c431783b658400f
Courtney Kimbrough <98043762+CourtneyKimbrough@users.noreply.github.com>
2025-09-19 21:10:50 +0000

    Initial commit

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,34 @@
+class DirectorsController < ApplicationController
+  def index
+    @list_of_directors = Director.all
+
+    render({ :template => "director_templates/index" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_directors = Director.where({ :id => the_id })
+
+    @the_director = matching_directors.at(0)
+    
+    render({ :template => "director_templates/show" })
+  end
+
+  def youngest
+    directors_with_dob = Director.where.not({ :dob => nil })
+    sorted_directors = directors_with_dob.order({ :dob => :desc })
+    @the_youngest_director = sorted_directors.at(0)
+
+    render({ :template => "director_templates/youngest" })
+  end
+
+  def oldest
+    directors_with_dob = Director.where.not({ :dob => nil })
+    sorted_directors = directors_with_dob.order({ :dob => :asc })
+    @the_oldest_director = sorted_directors.at(0)
+    
+    render({ :template => "director_templates/oldest" })
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/about", { :controller => "misc", :action => "about" })
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/eldest", { :controller => "directors", :action => "oldest" })
+  get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  get("/actors", { :controller => "actors", :action => "index" })
+  get("/actors/:path_id", { :controller => "actors", :action => "show" })
 end
